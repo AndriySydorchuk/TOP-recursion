2b8dd93388c7fb7f97649c8311d85abbe44ebac6
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-07 00:07:46 +0300

    Implement recursive fibonacci function and add tests for it

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -23,3 +23,23 @@ export function fibs(n) {
 
   return result;
 }
+
+export function fibsRec(n) {
+  if (!Number.isInteger(n) || n < 0) {
+    return undefined;
+  }
+
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+
+  const prevLastValue = prev[prev.length - 1];
+  const prevSecondLastValue = prev[prev.length - 2];
+
+  const curr = [...prev];
+  curr.push(prevLastValue + prevSecondLastValue);
+
+  return curr;
+}
--- fibonacci/fibonacci.test.js
+++ fibonacci/fibonacci.test.js
@@ -96,3 +96,100 @@ describe("Fibonacci using iteration", () => {
     expect(actual).toEqual(expected);
   });
 });
+
+describe("Fibonacci using recursion", () => {
+  let input;
+  let actual;
+  let expected;
+
+  test("0", () => {
+    input = 0;
+    actual = fibsRec(input);
+    expected = [];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("1", () => {
+    input = 1;
+    actual = fibsRec(input);
+    expected = [0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("2", () => {
+    input = 2;
+    actual = fibsRec(input);
+    expected = [0, 1];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("3", () => {
+    input = 3;
+    actual = fibsRec(input);
+    expected = [0, 1, 1];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("4", () => {
+    input = 4;
+    actual = fibsRec(input);
+    expected = [0, 1, 1, 2];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("5", () => {
+    input = 5;
+    actual = fibsRec(input);
+    expected = [0, 1, 1, 2, 3];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("6", () => {
+    input = 6;
+    actual = fibsRec(input);
+    expected = [0, 1, 1, 2, 3, 5];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("7", () => {
+    input = 7;
+    actual = fibsRec(input);
+    expected = [0, 1, 1, 2, 3, 5, 8];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("20", () => {
+    input = 20;
+    actual = fibsRec(input);
+    expected = [
+      0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
+      2584, 4181,
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("negative input", () => {
+    input = -3;
+    actual = fibsRec(input);
+    expected = undefined;
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("non-integer input", () => {
+    input = [9];
+    actual = fibsRec(input);
+    expected = undefined;
+
+    expect(actual).toEqual(expected);
+  });
+});
