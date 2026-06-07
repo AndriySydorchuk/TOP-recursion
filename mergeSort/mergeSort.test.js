f18f29307cf444cb38e67c119a3840f9806174f7
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-07 20:57:57 +0300

    Add more tests and refactor previous test names

--- mergeSort/mergeSort.test.js
+++ mergeSort/mergeSort.test.js
@@ -1,11 +1,11 @@
 import { mergeSort } from "./mergeSort";
 
-describe("merge sort test.skiping", () => {
+describe("merge sort", () => {
   let input;
   let actual;
   let expected;
 
-  test("sort 1", () => {
+  test("returns empty array for []", () => {
     input = [];
     actual = mergeSort(input);
     expected = [];
@@ -13,7 +13,7 @@ describe("merge sort test.skiping", () => {
     expect(actual).toEqual(expected);
   });
 
-  test("sort 2", () => {
+  test("returns same array for single item array", () => {
     input = [73];
     actual = mergeSort(input);
     expected = [73];
@@ -21,7 +21,7 @@ describe("merge sort test.skiping", () => {
     expect(actual).toEqual(expected);
   });
 
-  test("sort 3", () => {
+  test("returns sorted array for already sorted", () => {
     input = [1, 2, 3, 4, 5];
     actual = mergeSort(input);
     expected = [1, 2, 3, 4, 5];
@@ -29,7 +29,7 @@ describe("merge sort test.skiping", () => {
     expect(actual).toEqual(expected);
   });
 
-  test("sort 4", () => {
+  test("returns sorted array", () => {
     input = [3, 2, 1, 13, 8, 5, 0, 1];
     actual = mergeSort(input);
     expected = [0, 1, 1, 2, 3, 5, 8, 13];
@@ -37,10 +37,34 @@ describe("merge sort test.skiping", () => {
     expect(actual).toEqual(expected);
   });
 
-  test("sort 5", () => {
+  test("returns sorted array", () => {
     input = [105, 79, 100, 110];
     actual = mergeSort(input);
     expected = [79, 100, 105, 110];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("returns sorted array for unsorted with negative integers", () => {
+    input = [-3, 10, 0, -1];
+    actual = mergeSort(input);
+    expected = [-3, -1, 0, 10];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("returns sorted array for same integers array", () => {
+    input = [5, 5, 5];
+    actual = mergeSort(input);
+    expected = [5, 5, 5];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test("returns sorted array for array with same integers", () => {
+    input = [9, 1, 9, 3, 0, -5];
+    actual = mergeSort(input);
+    expected = [-5, 0, 1, 3, 9, 9];
 
     expect(actual).toEqual(expected);
   });
